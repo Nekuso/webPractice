5fe60ca159eabdb8a6b970404796d9fcbae3bd3d
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-04-06 22:22:15 +0800

    added npm and bootstrap dependencies

--- index.php
+++ index.php
@@ -110,7 +110,9 @@ $_SESSION["sales"] = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Activity 2</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <!-- Link Bootstrap from node_nodules -->
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.css">
 </head>
 
 <body style="display: flex; width: 100%; justify-content: center; align-items: flex-start; gap: 1rem; padding: 2rem;">
@@ -141,7 +143,8 @@ $_SESSION["sales"] = [
             ');
     }
     ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
